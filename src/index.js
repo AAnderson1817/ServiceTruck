f5480c82d11a289bdc6c1705ad44157efeeb0ca3
Andy Anderson <andyanderson1818@gmail.com>
2017-09-14 22:31:28 -0700

    Initial config setup for API usage

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import http from 'http';
+import bodyParser from 'body-parser';
+import express from 'express';
+import mongoose from 'mongoose';
+
+import config from './config';
+import routes from './routes';
+
+let app = express();
+app.server = http:createServer(app);
+
+//Middleware
+
+//Passport Config
+
+//API routes v1
+
+app.use('/v1', routes);
+
+app.server.listen(config.port);
+console.log(`Started on port ${app.server.address().port}`);
+
+export default app;
