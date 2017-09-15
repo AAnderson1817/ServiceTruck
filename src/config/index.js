10f87d9262c5a5ed8c44057f037c721284a50796
Andy Anderson <andyanderson1818@gmail.com>
2017-09-14 22:44:10 -0700

    Fixed db connection issues

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
 export default {
   "port": 3005,
-  "mongoUrl": "mongodb://<mugabe>:<robert>@ds135444.mlab.com:35444/servicetruck"
+  "mongoUrl": "mongodb://mugabe:robert@ds135444.mlab.com:35444/servicetruck"
 }
