75bda0edc6a875eba6df7d3ab893d1b69408b35b
Andy Anderson <andyanderson1818@gmail.com>
2017-09-18 09:58:16 -0700

    Completed CRUD! Module complete

--- src/controller/landmark.js
+++ src/controller/landmark.js
@@ -20,12 +20,63 @@ api.post('/add', (req, res) => {
   });
 });
 
+// '/v1/landmark' Get ALL
+
 api.get('/', (req,res) => {
   Landmark.find({}, (err, landmarks) => {
     if (err) {
       res.send(err);
     }
     res.json(landmarks);
+  });
+});
+// '/v1/landmark/:id' Get one by ID
+api.get('/:id', (req, res) => {
+  console.log(req.body);
+  Landmark.findById(req.params.id ,(err, landmark) =>{
+    if (err) {
+      res.send(err);
+    };
+    res.json(landmark);
+  });
+});
+
+// '/v1/landmark/:id' Update
+
+api.put('/:id', (req,res) => {
+  Landmark.findById(req.params.id, (err, landmark) => {
+    if(err){
+      res.send(err);
+    }
+    landmark.name = req.body.name;
+    landmark.save(err => {
+      if(err) {
+      res.send(err);
+    }
+    res.json({ message: "Landmark updated" });
+    });
+  });
+});
+
+api.get('/name', (req,res) => {
+  Landmark.find(req.params.name , (err, landmark) =>{
+  if(err) {
+    res.send(err);
+    };
+    res.send(landmark);
+  });
+});
+
+// '/v1/landmark/:id' Delete
+
+api.delete('/:id', (req,res) => {
+  Landmark.remove({
+    _id: req.params.id
+  }, (err, restaurant) => {
+    if(err) {
+      res.send(err);
+    }
+    res.json({ message: "Landmark destroyed! Call Department of the Homeland!"});
   });
 });
 
